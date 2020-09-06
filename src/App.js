cfe299419501ca7714785e89ae4dbbab4841c2e1
GerasimenkoAN <560560@bk.ru>
2020-09-06 17:46:55 +0500

    Убрал лишнее из проекта

--- src/App.js
+++ src/App.js
@@ -1,29 +1,20 @@
-import React from 'react';
-import "./styles/main.scss"
+import React from "react";
+import "./styles/main.scss";
 import Header from "./components/Header/Header";
-import {Switch, Route} from "react-router-dom";
-import Home from "./components/Home/Home";
-import FormikForm from "./components/FormikForm/FormikForm";
-import About from "./components/About/About";
+import { Switch, Route } from "react-router-dom";
 import Contacts from "./components/Contacts/Contacts";
-import BlogContainer from "./components/Blog/BlogContainer";
 import TaskListContainer from "./components/TaskList/TaskListContainer";
 
-
 function App() {
-    return (
-        <React.Fragment>
-            <Header/>
-            <Switch>
-                <Route exact path="/" component={Home}/>
-                <Route exact path="/formik" component={FormikForm}/>
-                <Route exact path="/task-list" component={TaskListContainer}/>
-                <Route exact path="/about" component={About}/>
-                <Route exact path="/contacts" component={Contacts}/>
-                <Route exact path="/blog" component={BlogContainer}/>
-            </Switch>
-        </React.Fragment>
-    );
+  return (
+    <React.Fragment>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={TaskListContainer} />
+        <Route exact path="/contacts" component={Contacts} />
+      </Switch>
+    </React.Fragment>
+  );
 }
 
 export default App;
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,7 +6,7 @@ import {Link} from "react-router-dom";
 const Header = (props) => {
     return (
         <>
-            <Navbar  collapseOnSelect expand="xl" bg="dark" variant="dark">
+            <Navbar collapseOnSelect expand="xl" bg="dark" variant="dark">
                 <Container>
                     <Navbar.Brand href="/">
                         <img src={reactlogo}
@@ -14,24 +14,16 @@ const Header = (props) => {
                              width="30"
                              className="d-inline-block align-top"
                              alt="Logo"
-                        />{' '}React Bootstrap!
+                        />{' '}Task List
                     </Navbar.Brand>
                     <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
                     <Navbar.Collapse id="responsive-navbar-nav">
                         <Nav className="mr-auto">
                             <Nav.Link as={Link} to="/"> Home </Nav.Link>
-                            <Nav.Link as={Link} to="/formik"> Formik </Nav.Link>
-                            <Nav.Link as={Link} to="/task-list"> Task list </Nav.Link>
-                            <Nav.Link as={Link} to="/about"> About us </Nav.Link>
                             <Nav.Link as={Link} to="/contacts"> Contacts </Nav.Link>
-                            <Nav.Link as={Link} to="/blog"> Blog </Nav.Link>
-
                         </Nav>
                     </Navbar.Collapse>
-                    <Form id="responsive-navbar-nav" inline className="d-sm-none d-none d-md-inline-block">
-                        <FormControl type="text" placeholder="Search" className="mr-sm-2"/>
-                        <Button variant="outline-info">Search</Button>
-                    </Form>
+
                 </Container>
             </Navbar>
 
